06c72dcd319374f723a56f09206aa694194259af
Drayce Chen <72390055+cfking94@users.noreply.github.com>
2021-09-17 13:59:35 +0800

    Update models.js

--- models/models.js
+++ models/models.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ipSchema = new Schema({
-  ip: {type: String, required: true},
-  liked: [{type: Schema.Types.ObjectId, ref: 'Stocks'}]
-});
-
 const stockSchema = new Schema({
   stock: {type: String, required: true},
   likes: {type: Number, default: 0}
 });
 
-const IPs = mongoose.model('IPs', ipSchema);
+const ipSchema = new Schema({
+  ip: {type: String, required: true},
+  liked: [{type: Schema.Types.ObjectId, ref: 'Stocks'}]
+});
+
 const Stocks = mongoose.model('Stocks', stockSchema);
+const IPs = mongoose.model('IPs', ipSchema);
 
 module.exports = {
   IPs: IPs,
